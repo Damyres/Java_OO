b812c00cfb60c7cfaa5de04b02eba56c1238c018
Damyres <damyresmaciell@gmail.com>
2023-05-27 18:33:21 -0300

    Praticando While/For

---
+++ src/criando/primeira/aplicacao/Programa.java
@@ -0,0 +1,19 @@
+package criando.primeira.aplicacao;
+
+public class Programa {
+
+    public static void main (String[] args){
+
+      /*  int contador = 1;
+
+        while(contador <=10){
+            contador++;
+            System.out.println(contador);
+        }
+*/
+        for(int contador =1; contador <=10; contador++ ){
+            System.out.println(contador);
+        }
+
+    }
+}

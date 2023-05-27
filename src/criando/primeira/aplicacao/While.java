1847162418d89e44f04366f48e73b1a45a6e7331
Damyres <damyresmaciell@gmail.com>
2023-05-27 17:46:06 -0300

    Praticando While

---
+++ src/criando/primeira/aplicacao/While.java
@@ -0,0 +1,27 @@
+package criando.primeira.aplicacao;
+
+import java.util.Scanner;
+
+public class While {
+    public static void main (String[] argd){
+
+        Scanner leitura = new Scanner(System.in);
+
+        int nota = 0;
+        double media = 0;
+        int totalDeNotas =0;
+
+        while (nota != -1){
+            System.out.println("Qual a sua avaliação para o filme ou digite -1 para encerrar: ");
+            nota = leitura.nextInt();
+            if(nota != -1){
+                media += nota;
+                totalDeNotas ++;
+            }
+        }
+
+        System.out.println("Média de valiações é " + media /totalDeNotas);
+
+    }
+
+}

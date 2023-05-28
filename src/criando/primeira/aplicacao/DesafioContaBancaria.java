0d71d539ca0534895c4c606de528e7a174603e6c
Damyres <damyresmaciell@gmail.com>
2023-05-28 11:12:59 -0300

    Continuação do projeto parte 2

--- src/criando/primeira/aplicacao/DesafioContaBancaria.java
+++ src/criando/primeira/aplicacao/DesafioContaBancaria.java
@@ -21,6 +21,53 @@ public class DesafioContaBancaria {
 
         System.out.println("**********************");
 
+        String operacoes = """
+                Operações:
+                1 - Consultar saldos
+                2 - Receber valor
+                3 - Transferir valor
+                4 - Sair
+                """;
+        System.out.println(operacoes);
+
+        /*System.out.println("Operações: ");
+        System.out.println("1 - Consultar saldos");
+
+        System.out.println("2 - Receber valor");
+
+        System.out.println("3 - Transferir valor");
+
+        System.out.println("4 - Sair");*/ //otimizando linhas de código
+
+        System.out.println("Digite a operação desejada:");
+        int numeroDigitado = leitor.nextInt();
+
+        if(numeroDigitado == 1){
+            System.out.println("Consultar saldos");
+        } else if(numeroDigitado ==2){
+            System.out.println("Receber valor");
+        } else if(numeroDigitado == 3){
+            System.out.println("Tranferir valor");
+        } else if (numeroDigitado ==4){
+            System.out.println("Sair");
+        } else {
+            System.out.println("Número inválido!");
+        }
+
+              /* if(numeroDigitado == 1){
+            System.out.println("Consultar saldos");
+        } else if(numeroDigitado ==2){
+            System.out.println("Receber valor");
+        } else if(numeroDigitado == 3){
+            System.out.println("Tranferir valor");
+        } else if (numeroDigitado ==4){
+            System.out.println("Sair");
+        } else {
+            System.out.println("Número inválido!");
+        }*/
+
+
+
 
     }
 

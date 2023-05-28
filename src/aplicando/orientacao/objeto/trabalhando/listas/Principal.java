47cfa40dbbe44c3d3d0b3fa1242e9708d2ab8ea0
Damyres <damyresmaciell@gmail.com>
2023-05-28 18:45:11 -0300

    Customizando linhas de código e acionando métodos

--- src/aplicando/orientacao/objeto/trabalhando/listas/Principal.java
+++ src/aplicando/orientacao/objeto/trabalhando/listas/Principal.java
@@ -8,13 +8,19 @@ public class Principal {
         filme1.anDeLancamento = 2009;
         filme1.duracaoEmMinuto = 95;
         filme1.incluindoNoPlano = true;
-        filme1.avaliacao = 7.9;
-        filme1.totalDeAvaliacao = 100;
+        filme1.somaDasAvaliacoes = 7.9;
+        filme1.totalDeAvaliacao = 3;
 
+        filme1.imprimiFichaTecnica();
 
-        System.out.println("Filme: " + filme1.nome);
-        System.out.println("Ano de lançamento: " + filme1.anDeLancamento);
-        System.out.println("Tempo: " + filme1.duracaoEmMinuto + " minutos. ");
+        filme1.avaliaFilme (10);
+        filme1.avaliaFilme(7);
+        filme1.avaliaFilme(7);
 
+        filme1.mediaDasAvaliacoes();
+
+        System.out.println("Soma de avaliações: " + filme1.somaDasAvaliacoes);
+        System.out.println("Número total de avaliações recebidas: " + filme1.totalDeAvaliacao);
+        System.out.println("Média das avaliações: " +filme1.mediaDasAvaliacoes());
     }
 }

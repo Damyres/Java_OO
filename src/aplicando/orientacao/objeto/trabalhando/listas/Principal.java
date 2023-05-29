36e6a1f0ddc20133a1e126034b548266d9e10e0b
Damyres <damyresmaciell@gmail.com>
2023-05-29 18:52:29 -0300

    Acionado métodos getters e setters pertencente a classe modelo em outro package

--- src/aplicando/orientacao/objeto/trabalhando/listas/Principal.java
+++ src/aplicando/orientacao/objeto/trabalhando/listas/Principal.java
@@ -1,15 +1,15 @@
 package aplicando.orientacao.objeto.trabalhando.listas;
 
+import aplicando.orientacao.objeto.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
 
         Filme filme1 = new Filme();
-        filme1.nome = "Cavaleiro sem cabeça";
-        filme1.anDeLancamento = 2009;
-        filme1.duracaoEmMinuto = 95;
-        filme1.incluindoNoPlano = true;
-        filme1.somaDasAvaliacoes = 7.9;
-        filme1.totalDeAvaliacao = 3;
+
+        filme1.setNome("Cavaleiro sem cabeça");
+        filme1.setAnDeLancamento(2009);
+        filme1.setDuracaoEmMinuto(95);
 
         filme1.imprimiFichaTecnica();
 
@@ -17,10 +17,11 @@ public class Principal {
         filme1.avaliaFilme(7);
         filme1.avaliaFilme(7);
 
-        filme1.mediaDasAvaliacoes();
 
-        System.out.println("Soma de avaliações: " + filme1.somaDasAvaliacoes);
-        System.out.println("Número total de avaliações recebidas: " + filme1.totalDeAvaliacao);
-        System.out.println("Média das avaliações: " +filme1.mediaDasAvaliacoes());
+        //System.out.println("Soma de avaliações: " + filme1.somaDasAvaliacoes);
+        //System.out.println("Número total de avaliações recebidas: " + filme1.totalDeAvaliacao);
+        System.out.println("Total de avaliações: "+ filme1.getTotalDeAvaliacao());
+        System.out.println("Média das avaliações: " + filme1.mediaDasAvaliacoes());
+
     }
 }
